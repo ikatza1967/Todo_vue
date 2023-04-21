5be3a6c343cf1c3f3d840f3f04fa6b53d275fb08
ikatza1967 <ikatzacarbon1@gmail.com>
2023-04-21 13:52:54 +0200

    editar implementado

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@ const contenido = ref("");
 
 const datosJson = ref("");
 
+const editfecha = ref("")
+const edittitulo = ref("")
+const editcontenido= ref("")
+
+
 //mostrar tareas
 
 let headersList = {
@@ -22,9 +27,6 @@ let reqOptions = {
 };
 
 
-
-
-
 //mostras tareas
 
 const getDatos = async () => {
@@ -36,51 +38,50 @@ getDatos();
 
 // crear post
 
-const CreatePost = async () => {
-  await axios({
-    method: "post",
-    url: "http://localhost:3000/tareas",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    data: {
+const CreatePost = () => {
+  axios.post("http://localhost:3000/tareas", {
       titulo: titulo.value,
       fecha: fecha.value,
       contenido: contenido.value,
-    },
-  });
+    });
 };
 
 // crear delete
 
 
-async function deleteTask(id){
+function deleteTask(id){
   const url = `http://localhost:3000/tareas/${id}`
-  const datosDelete = await axios.request(url,{
-    method: "DELETE",
-  headers: {'Content-type':'application/json'}
-  })
+  const datosDelete =  axios.delete(url)
  location.reload()
 }
 
 // editar comentario 
-let dataedit = JSON.stringify({
-  "fecha": "fecha.value",
-  "titulo": "titulo.value",
-  "contenido": "contenido.value"
-});
 
-async function editTask(id){
-  const editTask = await axios.put(`http://localhost:3000/tareas/${id}`,{
-    method: "PUT",
-  headers: {'Content-type':'application/json'},
-  data: dataedit
-  })
+
+const idTask = ref("")
+let mostrar = ref(false)
+
+function editTask(id){
+ idTask.value = id
+ mostrar.value = true
+ console.log("id", idTask.value)
 }
 
 
+function showEdit() {
+  try {
+    axios.put(`http://localhost:3000/tareas/${idTask.value}`, {
+      titulo: edittitulo.value,
+      fecha: editfecha.value,
+      contenido: editcontenido.value
+    })
+    location.reload()
 
+  } catch (error) {
+    console.log(error)
+  }
 
+}
 
 
 
@@ -107,7 +108,7 @@ async function editTask(id){
           rows="10"
           v-model="contenido"
         ></textarea>
-        <button type="submit" class="w-100" @click="CreatePost">guardar</button>
+        <button type="submit" class="w-100" @click="CreatePost()">guardar</button>
       </form>
     </div>
 
@@ -119,16 +120,30 @@ async function editTask(id){
         v-for="datos in datosJson"
         :key="datosJson.id"
       >
-        <h6  class="col-10">{{ datos.fecha }}</h6>
+        <h6  class="col-9">{{ datos.fecha }}</h6>
+        
+        
 
-       <button @click="deleteTask(datos.id)"> <i class="fa-solid fa-trash-can" ></i></button>
+        
+        <button @click="deleteTask(datos.id)"> <i class="fa-solid fa-trash-can" ></i></button>
 
-       <button @click="editTask(datos.id)"><i class="fa-solid fa-pen-to-square"></i> </button> 
+       <button @click="editTask(datos.id)" ><i class="fa-solid fa-pen-to-square"></i> </button> 
+       <h5  class="border rounded-1 col-12">{{ datos.titulo }} </h5>
+       
 
-        <h5  class="border rounded-1 col-12">{{ datos.titulo }} </h5>
+       
 
         <h6 class="border rounded-1 col-12">{{ datos.contenido }}</h6>
+        
       </div>
+      <div v-show="mostrar" >
+        <h4>Para editar </h4>
+        <input type="date" v-model="editfecha" >
+        <input type="text" v-model="edittitulo" >
+        <input type="text" v-model="editcontenido" >
+        <button  @click="showEdit()"><i class="fa-solid fa-check"></i></button>
+      </div>
+      
     </div>
   </div>
 </template>
